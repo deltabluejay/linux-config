6619b06e6837c632688b0b4e105d02872e8d34ff
deltabluejay <github-deltabluejay.iqoyw@slmail.me>
2023-12-20 02:18:54 -0700

    Updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#### Usage ####
 DISTROS=("SUSE" "FEDORA")
 
 # Check if the script is provided with an argument
@@ -12,10 +13,9 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Get the user argument
 DISTRO="$1"
 
-# Check if the argument is in the list of DISTROS
+# Check if argument is in the list of DISTROS
 if [[ " ${DISTROS[@]} " =~ " ${DISTRO} " ]]; then
     echo "Installing for $DISTRO... (3 seconds to abort)"
     sleep 3
@@ -30,17 +30,18 @@ else
 fi
 # TODO: implement different commands depending on distro
 
-###################
-#  SUSE
-###################
+####################
+#       SUSE       #
+####################
 
-### CONSTANTS ###
+#### CONSTANTS ####
 FIREFOX_EXTENSIONS=(
     "https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search" \
     "https://addons.mozilla.org/en-US/firefox/addon/1password-x-password-manager/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search" \
     "https://addons.mozilla.org/en-US/firefox/addon/simplelogin/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search" \
     "https://addons.mozilla.org/en-US/firefox/addon/darkreader/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search" \
     "https://addons.mozilla.org/en-US/firefox/addon/xbs/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search" \
+    "https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm" \
 )
 
 PACKAGES_REPO=(
@@ -77,18 +78,17 @@ FLATPAKS=(
 )
 
 
-### SETUP FILE STRUCTURE ###
+#### SETUP FILE STRUCTURE ####
 echo "Setting up file structure..."
 mkdir ./downloads
 mkdir $HOME/Applications
-git clone <INSERT GITHUB URL HERE> # TODO: get link
 
 # .bash_profile and .bashrc
 cat ./files/.bash_profile_additions >> $HOME/.bash_profile
 cat ./files/.bashrc_additions >> $HOME/.bashrc
 
 
-### INSTALL APPLICATIONS ###
+#### INSTALL APPLICATIONS ####
 # Install packages from repository
 ZYPPER_COMMAND="sudo zypper in -y"
 echo "Installing packages..."
@@ -142,7 +142,6 @@ echo "Installing Github CLI..."
 sudo zypper addrepo https://cli.github.com/packages/rpm/gh-cli.repo
 sudo zypper ref
 sudo zypper in -y gh
-# TODO: login with Github (`gh auth login`)
 
 # Pyenv
 echo "Installing Pyenv..."
@@ -157,16 +156,16 @@ wal --theme ./dots/pywal/themes/hackthebox.theme
 # Obsidian
 echo "Installing Obsidian..."
 wget -O ./downloads/Obsidian.AppImage https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/Obsidian-1.4.16.AppImage
-# TODO: AppImage installing tool
+# idk what directory this will default install to
+ail-cli integrate ./downloads/Obsidian.AppImage
 
 # Ghidra
-wget -O ./downloads/ghidra.zip https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.4_build/ghidra_10.4_PUBLIC_20230928.zip
-unzip ./downloads/ghidra.zip
-chmod +x ./downloads/ghidra/ghidraRun
-mv ./downloads/ghidra $HOME/Applications/Ghidra
+wget -O ./downloads/Ghidra.zip https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.4_build/ghidra_10.4_PUBLIC_20230928.zip
+unzip ./downloads/Ghidra.zip
+chmod +x ./downloads/Ghidra/ghidraRun
+mv ./downloads/Ghidra $HOME/Applications/
 mkdir $HOME/.local/share/applications
 cp ./files/Ghidra.desktop $HOME/.local/share/applications
-./$HOME/Applications/Ghidra/ghidraRun # probably not right
 
 # GEF
 echo "Installing GEF for GDB..."
@@ -174,32 +173,48 @@ bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
 
 # Burp Suite
 wget -O ./downloads/burpsuite_install.sh https://portswigger-cdn.net/burp/releases/download?product=pro&version=2023.11.1.3&type=Linux
-./downloads/burpsuite_install.sh # probably not right
+chmod +x ./downloads/burpsuite_install.sh
 
-# TODO: Other VPNs
-# TODO: install media codecs?
-# TODO: configure power saving?
+#### USER SETUP ####
+echo "These parts require user interaction..."
+source ./downloads/burpsuite_install.sh
+source ./$HOME/Applications/Ghidra/ghidraRun
+gh auth login
 
 # Install Firefox extensions (open links automatically)
 firefox --new-tab https://addons.mozilla.org/en-US/firefox/addon/1password-x-password-manager/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search
 for url in "${EXTENSIONS[@]}"; do
   firefox --new-tab "$url"
 done
-# TODO: Firefox theme
-# TODO: configure Ublock Origin lists
 
+# TODO: Other VPNs
+# TODO: Install media codecs?
+# TODO: Configure power saving?
 
 echo "TODO:"
-echo "    -[ ] KDE customization"
-echo "    -[ ] Login to applications (1Password, gh, Obsidian, Discord, Slack, XBrowserSync, SimpleLogin)"
-echo "    -[ ] Install Firefox extensions and theme"
-echo "    -[ ] Configure Firefox preferences"
-echo "    -[ ] Configure Ublock Origin filter lists"
-echo "    -[ ] Fully set up Ghidra"
-echo "    -[ ] Ricing"
-echo "        -[ ] VSCodium theme"
-echo "        -[ ] KDE theme"
-echo "        -[ ] Obsidian theme"
-echo "        -[ ] Wallpaper"
-echo "        -[ ] "
-echo "    -[ ] "
+echo "  - [ ] KDE customization"
+echo "    - [ ] Latte Dock"
+echo "    - [ ] Workspaces and keybindings"
+echo "    - [ ] Auto brightness off"
+echo "    - [ ] Display scaling?"
+echo "    - [ ] Dark mode"
+echo "  - [ ] Login to applications"
+echo "    - [ ] 1Password (desktop and browser)"
+echo "    - [ ] Obsidian"
+echo "    - [ ] Discord"
+echo "    - [ ] Slack"
+echo "    - [ ] XBrowserSync"
+echo "    - [ ] SimpleLogin"
+echo "    - [ ] ProtonVPN"
+echo "  - [ ] Install Firefox theme"
+echo "  - [ ] Configure Firefox preferences"
+echo "    - [ ] Compact mode"
+echo "    - [ ] Security/privacy settings"
+echo "  - [ ] Configure Ublock Origin filter lists"
+echo "  - [ ] Fully set up Ghidra"
+echo "  - [ ] Ricing"
+echo "    - [ ] VSCodium theme"
+echo "    - [ ] KDE theme"
+echo "    - [ ] Obsidian theme"
+echo "    - [ ] Wallpaper"
+echo "  - [ ] Install Steam games
